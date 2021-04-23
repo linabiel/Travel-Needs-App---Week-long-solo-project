f0fa6691b38e6bde0762bfdff45cbd32b32c22b6
linabiel <linuka.b@gmail.com>
2021-04-23 13:48:10 +0100

    user class created

---
+++ models/user.py
@@ -0,0 +1,8 @@
+class User:
+
+    def __init__(self, name, home_city, home_country, id = NONE):
+        self.id = id
+        self.name = name
+        self.home_city = home_city
+        self.home_country = home_country
+        destination = []

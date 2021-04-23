bbc6f87117e2ce8ae4206976c4334419075cc00d
linabiel <linuka.b@gmail.com>
2021-04-23 14:16:15 +0100

    create console for user

---
+++ console.py
@@ -0,0 +1,21 @@
+from models.city import City
+from models.user import User
+from models.country import Country
+
+
+import repositories.user_repository as user_repository
+
+
+user_repository.delete_all()
+
+user1 = User('Lina', 'Edinburgh', 'Scotland')
+user_repository.save(user1)
+
+user2 = User('Oscar Wilde', 'Dublin', 'Ireland')
+user_repository.save(user2)
+
+user3 = User('Peter Parker', 'New York', 'USA')
+user_repository.save(user3)
+
+
+

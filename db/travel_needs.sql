4fa9eb7198940dd836ed08fdfd74b0592827c1b6
linabiel <linuka.b@gmail.com>
2021-04-23 13:21:21 +0100

    db tables created

--- db/travel_needs.sql
+++ db/travel_needs.sql
@@ -1,1 +1,32 @@
-DROP TABLE
+DROP TABLE destinations;
+DROP TABLE cities;
+DROP TABLE countries;
+DROP TABLE users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    home_city VARCHAR(255),
+    home_country VARCHAR(255),
+    destination VARCHAR(255)
+)
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    user_id INT REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE
+)
+
+CREATE TABLE destinations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    city_id INT REFERENCES cities(id) ON DELETE CASCADE,
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE
+)
